7f9570e42242e9341535293dac510d805985f176
TEHSEEN RAZA <94171982+Haanshacker@users.noreply.github.com>
2025-11-24 16:52:17 +0500

    Create csrf.php

---
+++ tehseenraza-premium/core/core/csrf.php
@@ -0,0 +1,18 @@
+<?php
+// core/csrf.php
+if (session_status() === PHP_SESSION_NONE) session_start();
+
+function csrf_token() {
+    if (empty($_SESSION['csrf_token']))
+        $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+    return $_SESSION['csrf_token'];
+}
+
+function csrf_input() {
+    return '<input type="hidden" name="csrf_token" value="'.htmlspecialchars(csrf_token()).'">';
+}
+
+function csrf_verify($token) {
+    if (!isset($_SESSION['csrf_token'])) return false;
+    return hash_equals($_SESSION['csrf_token'], $token);
+}
